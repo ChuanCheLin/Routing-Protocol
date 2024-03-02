11973260ddd68ffaa4e94aaf908896484e3f93da
ChuanCheLin <diamond16888@gmail.com>
2024-03-01 18:44:02 -0600

    Read and Parse the Topology File, using self defined Graph to represent the network topology

--- distvec.cpp
+++ distvec.cpp
@@ -1,18 +1,51 @@
 #include <cstdio>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <utility> // for std::pair
+
+// Assuming nodes are numbered from 1 to N and the graph is undirected
+// Using vector of vectors to represent an adjacency list, where each pair is (neighbor, cost)
+typedef std::vector<std::vector<std::pair<int, int>>> Graph;
 
 int main(int argc, char** argv) {
-    //printf("Number of arguments: %d", argc);
     if (argc != 4) {
-        printf("Usage: ./distvec topofile messagefile changesfile\n");
+        std::cout << "Usage: ./distvec topofile messagefile changesfile\n";
         return -1;
     }
 
-    FILE *fpOut;
-    fpOut = fopen("output.txt", "w");
-    fclose(fpOut);
+    std::ifstream topoFile(argv[1]);
+    if (!topoFile.is_open()) {
+        std::cerr << "Could not open topology file\n";
+        return -1;
+    }
 
-    // You may choose to use std::fstream instead
-    // std::ofstream fpOut("output.txt");
+    Graph graph;
+    int node1, node2, cost;
+
+    // Update this part to correctly initialize your graph based on the maximum node ID
+    // For now, let's assume a fixed size graph
+    graph.resize(100);
+
+    while (topoFile >> node1 >> node2 >> cost) {
+        graph[node1].push_back(std::make_pair(node2, cost));
+        graph[node2].push_back(std::make_pair(node1, cost)); // because the graph is undirected
+    }
+
+    topoFile.close();
+
+    // debug the topology
+    // for (int i = 0; i < graph.size(); ++i) {
+    //     if (!graph[i].empty()) {
+    //         std::cout << "Node " << i << " -> ";
+    //         for (const auto& neighbor : graph[i]) {
+    //             std::cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
+    //         }
+    //         std::cout << "\n";
+    //     }
+    // }
+
+    // You can now proceed with implementing the Distance Vector algorithm using the populated graph
 
     return 0;
 }
